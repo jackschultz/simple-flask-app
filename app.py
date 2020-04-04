10cee9c8d51f1ff516abb11db3ec2a98f73664db
Jack Schultz <jackschultz23@gmail.com>
2020-04-04 15:36:10 -0500

    adding templates for html and sqlalchemy for queries

--- app.py
+++ app.py
@@ -1,38 +1,46 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template, jsonify
 from flask import request, abort
-
-from sqlalchemy import create_engine
+from flask_sqlalchemy import SQLAlchemy
 
 NUM_PER_PAGE = 4
-DB_URL = os.environ.get('DB_URL')
-
-engine = create_engine(DB_URL)
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_URL')
+db = SQLAlchemy(app)
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200))
+    body = db.Column(db.String)
+    posted_at = db.Column(db.DateTime)
+
+    def __repr__(self):
+        return f"<Post id={self.id}, title={self.title}>"
+
+    def serialize(self):
+        return {'id': self.id,
+                'title': self.title,
+                'body': self.body,
+                'posted_at': self.posted_at
+                }
 
 
 @app.route('/posts/')
 def post_index():
-    page = int(request.args.get('page') or '0')
-    index = (NUM_PER_PAGE + 1) - (NUM_PER_PAGE * page)
-    offset = NUM_PER_PAGE * index if index > 0 else 1000 # never going to have 4000 posts
-
-    with engine.connect() as connection:
-        result = connection.execute(f"select * from posts order by id desc limit {NUM_PER_PAGE} offset {offset}").fetchall()
-        pages = [dict(res) for res in result]
-
-    retval = {'posts': pages}
-    return retval
+    page_num = int(request.args.get('page') or '1')
+    post_objs = Post.query.order_by(Post.id.desc()).paginate(page_num, NUM_PER_PAGE)
+    return render_template('posts/index.html', posts=post_objs.items)
 
 
 @app.route('/posts/<post_id>')
-def post_get(post_id):
+def post_show(post_id):
     int_post_id = int(post_id) # yes yes yes, sql injection
-    with engine.connect() as connection:
-        post = connection.execute(f"select * from posts where id = {int_post_id}").fetchone()
-        return dict(post)
+    post = Post.query.get_or_404(int_post_id)
+    return render_template('posts/show.html', post=post)
 
 
 if __name__ == '__main__':
